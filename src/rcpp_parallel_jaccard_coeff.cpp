9fcfd666ae133b94ddf90f37c69323fd739633f7
jeky82 <gambardella@tigem.it>
2019-08-14 16:32:31 +0200

    simplify parallell jaccard and since we already use it. Use Eigen matrix for now

--- src/rcpp_parallel_jaccard_coeff.cpp
+++ src/rcpp_parallel_jaccard_coeff.cpp
@@ -1,23 +1,24 @@
 #include <Rcpp.h>
 #include <RcppParallel.h>
+#include <RcppEigen.h>
 
 // Compute jaccard coefficient between nearest-neighbor sets in parallell
 //
 // Author: Gennaro Gambardella, Date: 12/08/2019
 
-
+// [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::depends(RcppParallel)]]
 struct JCoefficient : public RcppParallel::Worker {
   
   // input matrix to read from
-  const RcppParallel::RMatrix<double> mat;
+  const Rcpp::NumericMatrix mat;
   
   // output matrix to write to
-  RcppParallel::RMatrix<double> rmat;
+  Eigen::MatrixXd& rmat;
   
   // initialize from Rcpp input and output matrixes (the RMatrix class
   // can be automatically converted to from the Rcpp matrix type)
-  JCoefficient(const Rcpp::NumericMatrix& mat, Rcpp::NumericMatrix& rmat)
+  JCoefficient(const Rcpp::NumericMatrix& mat, Eigen::MatrixXd& rmat)
     : mat(mat), rmat(rmat) {}
   
   // function call operator that work for the specified range (begin/end)
@@ -26,20 +27,10 @@ struct JCoefficient : public RcppParallel::Worker {
       for (std::size_t j = 0; j < mat.ncol(); j++) {
         
         int k = mat(i,j)-1;
-        std::vector<double> v(mat.ncol());
-
-        RcppParallel::RMatrix<double>::Row v1 = mat.row(i);
-        RcppParallel::RMatrix<double>::Row v2 = mat.row(k);
         
-        std::sort(v1.begin(), v1.end());
-        std::sort(v2.begin(), v2.end());
-        
-        std::vector<int> v_intersection;
-        
-        std::set_intersection(v1.begin(), v1.end(),
-                              v2.begin(), v2.end(),
-                              std::back_inserter(v_intersection));
-        int u = v_intersection.size();
+        Rcpp::NumericVector nodei = mat(i,Rcpp::_);
+        Rcpp::NumericVector nodej = mat(k,Rcpp::_);
+        int u = intersect(nodei, nodej).size();  // count intersection number
 
         if(u>0){
           rmat((i*mat.ncol())+j, 0) = i+1;
@@ -52,16 +43,15 @@ struct JCoefficient : public RcppParallel::Worker {
 };
 
 // [[Rcpp::export]]
-Rcpp::NumericMatrix rcpp_parallel_jaccard_coef(Rcpp::NumericMatrix mat, bool printOutput) {
+Eigen::MatrixXd rcpp_parallel_jaccard_coef(Rcpp::NumericMatrix mat, bool printOutput) {
   
   if (printOutput)
   {
     Rprintf("Running Parallell Jaccard Coefficient Estimation...\n");
   }
-  
   // allocate the matrix we will return
-  Rcpp::NumericMatrix rmat(mat.nrow()*mat.ncol(),3);
-  
+  Eigen::MatrixXd rmat = Eigen::MatrixXd::Zero(mat.nrow()*mat.ncol(),3);
+
   // create the worker
   JCoefficient JCoefficient(mat, rmat);
   
