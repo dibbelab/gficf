e585234ab2837bf1c0f31fe9f7e6861c0e9ceddc
jeky82 <gambardella@tigem.it>
2019-08-23 00:52:27 +0200

    add a way to add symbols to a dataframe with ensamble id only

--- R/util.R
+++ R/util.R
@@ -37,3 +37,46 @@ tsmessage <- function(..., domain = NULL, appendLF = TRUE, verbose = TRUE,time_s
     utils::flush.console()
   }
 }
+
+#' Convert Ensamble IDs to Official Gene Symbols
+#'
+#' It uses biomart. If more the one gene is associated to the enamble, the first one retrived from
+#' Biomart is used.
+#' 
+#' @param df data frame; Data frame containing the IDs to convert.
+#' @param col characters; Name of column containing the ensamble ids.
+#' @param organism characters; Organism of origin (i.e. human or mouse).
+#' @return The updated data frame with a new column called symb.
+#'
+#' @importFrom  biomaRt useMart getBM
+#' @import fastmatch
+#' 
+#' @export
+ensToSymbol = function(df,col,organism,verbose=T)
+{
+  organism = base::match.arg(arg = organism,choices = c("human","mouse"),several.ok = F)
+  
+  if(organism %in% "human")
+  {
+    tsmessage(".. Start converting human ens to human symbol",verbose = verbose)
+    g = unique(as.character(df[,col]))
+    ensembl = biomaRt::useMart("ensembl",dataset="hsapiens_gene_ensembl")
+    ens.map = biomaRt::getBM(attributes=c('ensembl_gene_id','hgnc_symbol'),filters = 'ensembl_gene_id',values = g,mart = ensembl,verbose = F)
+    df$symb = NA
+    df$symb = ens.map$hgnc_symbol[fastmatch::fmatch(df[,col],ens.map$ensembl_gene_id)]
+    tsmessage("Done!",verbose = verbose)
+  }
+  
+  if (organism %in% "mouse")
+  {
+    tsmessage(".. Start converting human symbols to mouse ensamble id",verbose = verbose)
+    g = as.character(unique(unlist(pathways)))
+    ensembl = biomaRt::useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+    ens.map = biomaRt::getBM(attributes=c('ensembl_gene_id','mgi_symbol'),filters = 'ensembl_gene_id',values = g,mart = ensembl,verbose = F)
+    df$symb = NA
+    df$symb = ens.map$mgi_symbol[fastmatch::fmatch(df[,col],ens.map$ensembl_gene_id)]
+    tsmessage("Done!",verbose = verbose)
+  }
+  
+  return(df)
+}
